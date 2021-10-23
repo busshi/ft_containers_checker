13fee80ed22e306e033f2ead0e01c64d72bbbba3
aldubar <aldubar@student.42.fr>
2021-10-23 22:25:18 +0200

    add vector swap

---
+++ srcs/vector/14_swap.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   14_swap.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 16:44:16 by aldubar           #+#    #+#             */
+/*   Updated: 2021/10/23 22:23:39 by aldubar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cont.hpp"
+
+template< class T>
+void	run( T v1, T v2, std::string s ) {
+
+	std::cout << "🔽  BEFORE SWAP..." << std::endl;
+	std::cout << "Vector 1: " << std::endl;
+	display(v1, s);
+	std::cout << "Vector 2: " << std::endl;
+	display(v2, s);
+
+	std::cout << "🔽  AFTER SWAP..." << std::endl;
+	v1.swap(v2);
+	std::cout << "Vector 1: " << std::endl;
+	display(v1, s);
+	std::cout << "Vector 2: " << std::endl;
+	display(v2, s);
+
+	std::cout << "-------------------------" << std::endl;
+}
+
+int		main( void ) {
+
+	Cont	cont;
+
+	alarm(3);
+
+	std::cout << "\033[33m[+] Testing iterators...\033[0m" << std::endl << std::endl;
+
+	run(cont.vs2, cont.vs3, STR);
+
+	run(cont.vi2, cont.vi3, INT);
+
+	run(cont.vc2, cont.vc3, CHAR);
+
+	return 0;
+}
