677e812bf7ea0e03079f5c81be0f0e88959c1d60
aldubar <aldubar@student.42.fr>
2021-11-01 10:47:35 +0100

    add ft::swap test for vector

--- srcs/vector/17_swap.cpp
+++ srcs/vector/17_swap.cpp
@@ -6,7 +6,7 @@
 /*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:44:16 by aldubar           #+#    #+#             */
-/*   Updated: 2021/11/01 10:06:53 by aldubar          ###   ########.fr       */
+/*   Updated: 2021/11/01 10:46:45 by aldubar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	run( T v1, T v2, std::string s ) {
 	display(v2, s);
 
 	std::cout << "🔽  AFTER SWAP BACK..." << std::endl;
-	std::swap(v1, v2);
+	swap(v1, v2);
 	std::cout << "Vector 1: " << std::endl;
 	display(v1, s);
 	std::cout << "Vector 2: " << std::endl;
