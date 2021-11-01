bcd13444d99649daf12da61e092ce5bebe10f978
aldubar <aldubar@student.42.fr>
2021-11-01 10:07:49 +0100

    swap for vector with std specialization

--- srcs/vector/17_swap.cpp
+++ srcs/vector/17_swap.cpp
@@ -6,7 +6,7 @@
 /*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:44:16 by aldubar           #+#    #+#             */
-/*   Updated: 2021/10/24 00:10:59 by aldubar          ###   ########.fr       */
+/*   Updated: 2021/11/01 10:06:53 by aldubar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@ void	run( T v1, T v2, std::string s ) {
 
 	std::cout << "🔽  AFTER SWAP..." << std::endl;
 	v1.swap(v2);
+	std::cout << "Vector 1: " << std::endl;
+	display(v1, s);
+	std::cout << "Vector 2: " << std::endl;
+	display(v2, s);
+
+	std::cout << "🔽  AFTER SWAP BACK..." << std::endl;
+	std::swap(v1, v2);
 	std::cout << "Vector 1: " << std::endl;
 	display(v1, s);
 	std::cout << "Vector 2: " << std::endl;
